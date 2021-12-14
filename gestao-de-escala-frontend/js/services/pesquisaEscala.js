cefd2f6e040e2bc88cb072d281fa47c7bdb9063f
mlobato447 <mlobato447@gmail.com>
2021-12-14 09:55:27 -0300

    Revert "Merge branch 'merge-cadu' into 'main'"
    This reverts commit ef9176196ae0f71bcc2c399c165fc8e08bab8bfd, reversing
    changes made to 372e5f5e04e4ac2ff1e4b4c420a3d5c2f2985e46.

--- gestao-de-escala-frontend/js/services/pesquisaEscala.js
+++ gestao-de-escala-frontend/js/services/pesquisaEscala.js
@@ -19,7 +19,6 @@ var escalaJson = {
 var pautaJson = {
   // "id": "",
    "data": "",
-   "data2": "",
    "hora": "",
    "sala": "",
    "processo": "",
@@ -32,18 +31,13 @@ var pautaJson = {
 }
 
 window.onload = function() {
-  var option='<option selected>Todas com mutirão</option>';
-
-  $('#vara').html(option).show();
-  listarVaras();
 
   axios.get(baseURL + 'pautas/').then(response => {
     pautas = response.data;
-    //console.log(pautas);
     pautas.forEach(listar);
     
-    //verificarSeTemEscala(pautas);
-    //getMutirao();
+    verificarSeTemEscala(pautas);
+    getMutirao();
     advogadoListar()
   }).catch(error => console.error(error));
 
@@ -63,26 +57,22 @@ window.onload = function() {
   }).catch(error => console.error(error));
 };
 
-function listarVaras(){
+function listarVaras(vara){
   axios.get(baseURL + 'mutiroes/').then(response => {
-    var mutirao = response.data;
-    muti(mutirao);
-
-    // mutirao =  mutirao.filter(item =>  item.id == idMutirao);
-    // mutirao = mutirao[0];
-    // if(mutirao){
-    //   if(mutirao.status == "COM_ESCALA")
-    //     btnGerar.style.display = 'none';
-    //   else{
-    //     btnGerar.style.display = 'inline';
-    //     //btnEditar.style.display = 'none';
-    //   }
-    // }
-
-    // pautas = pautas.filter(item =>  item.vara == vara);
-  console.log("Varas com mutirão");
-  console.log(varasComMutirao);
-
+    var mutiroes = response.data;
+    var option='<option selected>'+vara+'</option>';
+  
+    mutiroes = mutiroes.filter(item =>  item.status == "COM_ESCALA");
+  
+    $.each(mutiroes, function(i, obj){
+     
+      option += '<option value="'+obj.vara+'">'+obj.vara+'</option>';
+  
+    }) 
+    $('#vara').html(option).show(); 
+         // $('#mensagem').html('<span class="mensagem">Total de paises encontrados.: '+dados.length+'</span>');
+   
+ 
   }).catch(error => console.error(error));
 }
 
@@ -93,12 +83,8 @@ function listar(pautas){
   }else{
     if( procurador.nomeProcurador)
       pautas.procurador = procurador.nomeProcurador;
-  }
-  //  pautas.processo = 1234
-  // pautas.processo = pautas.processo.replace(/(\d)(\d{3}[\.,])/,"$1.$2")
-  
-  pautas.data = formatarData(pautas.data, "-");
-  //console.log(pautas.data)
+      pautas.data = formatarData(pautas.data, "-");
+  // console.log(pautas.data)
     var tabela = $('#dataTablePesquisa').DataTable();
     tabela.row.add( [
       pautas.data,
@@ -113,6 +99,11 @@ function listar(pautas){
       pautas.procurador
 
     ] ).draw( false );
+  }
+  //  pautas.processo = 1234
+  // pautas.processo = pautas.processo.replace(/(\d)(\d{3}[\.,])/,"$1.$2")
+  
+
 }
 
 function advogadoListar(){
@@ -125,7 +116,6 @@ function advogadoListar(){
       advogadoDaLista = advogadoDaLista[0];
       // console.log(nomeAdvogado)
       if(advogadoDaLista){
-        console.log(advogadoDaLista)
         $(this).css('color', 'red');
       }
     }
@@ -159,30 +149,14 @@ function pesquisar(pautaJson){
   var tabela = $('#dataTablePesquisa').DataTable();
   var pautaDaPesquisa; 
   var procurador = pautaJson.procurador;
+  console.log(pautaJson);
   axios.get(baseURL + 'pautas/').then(response => {
     var pautas = response.data;
     pautaDaPesquisa = pautas;
-    if(pautaJson.data2 && pautaJson.data) {
-      if( pautaJson.data <= pautaJson.data2){
 
-        pautaDaPesquisa = pautaDaPesquisa.filter(item => {
-          let dataInicial = parseInt(pautaJson.data.split("-").join(''),10);
-          let dataFinal = parseInt(pautaJson.data2.split("-").join(''),10);
-          let data = parseInt(item.data.split("-").join(''),10);
-          return (data >= dataInicial && data <= dataFinal);
-        }
-        );
-      } else {
-        alert("Data inicial é maior que data final. Verifique.");
-        return;
-      }
-     
-    }
-
-    else if(pautaJson.data){
+    if(pautaJson.data){
       pautaDaPesquisa = pautaDaPesquisa.filter(item =>  item.data == pautaJson.data);
     }
-  
 
     if(pautaJson.hora){
       pautaDaPesquisa = pautaDaPesquisa.filter(item =>  item.hora == pautaJson.hora);
@@ -192,13 +166,11 @@ function pesquisar(pautaJson){
       pautaDaPesquisa = pautaDaPesquisa.filter(item =>  item.sala == pautaJson.sala);
     }
     
-    if(pautaJson.vara && !(pautaJson.vara === "Todas as varas")){
+    if(pautaJson.vara){
       pautaDaPesquisa = pautaDaPesquisa.filter(item =>  item.vara == pautaJson.vara);
     }
 
-    pautaDaPesquisa = pautaDaPesquisa.filter(item => item.procurador != null);
-
-    if(procurador.nomeProcurador && !(procurador.nomeProcurador === "Todos os pautistas")){
+    if(procurador.nomeProcurador){
       pautaDaPesquisa = pautaDaPesquisa.filter(item =>  item.procurador.nomeProcurador == procurador.nomeProcurador);
     }
 
@@ -224,39 +196,43 @@ function gerarPorVara(escala){
   }).catch(error => console.error(error));
 }
 
-// function getMutirao(){
-//   //$('#mensagem').html('<span class="mensagem">Aguarde,carregando ...</span>');
+function getMutirao(){
+  //$('#mensagem').html('<span class="mensagem">Aguarde,carregando ...</span>');
   
-//   var vara = document.getElementById('vara');
-//   vara = vara.options[vara.selectedIndex].value; 
-//   selectVara = vara;
+  var vara = document.getElementById('vara');
+  vara = vara.options[vara.selectedIndex].value; 
+  selectVara = vara;
 
-//   axios.get(baseURL + 'mutiroes/').then(response => {
-//     mutiroes = response.data;
-//     mutiroes = mutiroes.filter(item =>  item.vara == vara);
-    
-//     if(mutiroes){
-//       listarMutirao(mutiroes);
-//       exibirBtnGerar();
-//     }
+  axios.get(baseURL + 'mutiroes/').then(response => {
+    mutiroes = response.data;
+    mutiroes = mutiroes.filter(item =>  item.vara == vara);
 
-//   }).catch(error => console.error(error));
 
-//   // axios.get(baseURL + 'pautas/').then(response => {
-//   //   pautas = response.data;
-//   //   pautas = pautas.filter(item =>  item.vara == vara);
-//   //   if(pautas.length < 1)
-//   //     pautas = response.data;
-//   //   listaEspecificaListar(pautas)
+    mutiroes = mutiroes.filter(item =>  item.status == "COM_ESCALA"); 
+    console.log(mutiroes)
     
-//   // }).catch(error => console.error(error));
+    if(mutiroes){
+      listarMutirao(mutiroes);
+      // exibirBtnGerar();
+    }
+
+  }).catch(error => console.error(error));
+
+  // axios.get(baseURL + 'pautas/').then(response => {
+  //   pautas = response.data;
+  //   pautas = pautas.filter(item =>  item.vara == vara);
+  //   if(pautas.length < 1)
+  //     pautas = response.data;
+  //   listaEspecificaListar(pautas)
     
-// }
+  // }).catch(error => console.error(error));
+    
+}
 
 function selectPautistas(procuradores){
 
   if (procuradores){
-    var option = '<option>Todos os pautistas</option>';
+    var option = '<option></option>';
     $.each(procuradores, function(i, obj){
       option += '<option value="'+obj.nomeProcurador+'">'+obj.nomeProcurador+'</option>';
     })
@@ -308,54 +284,27 @@ function exibirPorMutirao(){
     
 }
 
-function muti(mutiroes){
-  console.log(mutiroes)
-  var option = '<option>Todas as varas</option>';
-  
-  var naoTem =  true;
-  varasComMutirao[0] = mutiroes[0].vara;
-
-  $.each(mutiroes, function(i, obj){
-    console.log(i)
-    if(i == 0){
-      option += '<option value="'+obj.vara+'">'+obj.vara+'</option>';
-    }
-    varasComMutirao.forEach(function(vara){
-      if(obj.vara == vara){
-        naoTem = false;
-      }else{
-        naoTem = true;
-      }
-    });
-    if(naoTem){
-      varasComMutirao.push(obj.vara);
-      option += '<option value="'+obj.vara+'">'+obj.vara+'</option>';
-    }
-  }) 
-  $('#vara').html(option).show(); 
-}
-
-// function listarMutirao(mutiroes){
-//   if (mutiroes.length > 0){
-//     var option='';
-//     $.each(mutiroes, function(i, obj){
-//       option += '<option value="'+obj.id+'">'+ formatarData(obj.dataInicial, "/") +' a '+ formatarData(obj.dataFinal, "/") +'</option>';
-//     })
-//     //mutiroes.forEach(exibirMutirao);
+function listarMutirao(mutiroes){
+  if (mutiroes.length > 0){
+    var option='';
+    $.each(mutiroes, function(i, obj){
+      option += '<option value="'+obj.id+'">'+ formatarData(obj.dataInicial, "/") +' a '+ formatarData(obj.dataFinal, "/") +'</option>';
+    })
+    //mutiroes.forEach(exibirMutirao);
     
-//   }else{
-//     option = '<option selected>Vara selecionada sem Mutirão</option>';
-//   }
-//        // $('#mensagem').html('<span class="mensagem">Total de paises encontrados.: '+dados.length+'</span>');
-//   $('#mutirao').html(option).show();
+  }else{
+    option = '<option selected>Vara selecionada sem Mutirão</option>';
+  }
+       // $('#mensagem').html('<span class="mensagem">Total de paises encontrados.: '+dados.length+'</span>');
+  $('#mutirao').html(option).show();
 
-//   var mutirao = document.getElementById('mutirao');
-//   if(mutirao){  
-//     mutirao = mutirao.options[mutirao.selectedIndex].value; 
-//     selectVara = mutirao;
-//     //listarMutirao(mutirao);
-//   }
-// }
+  var mutirao = document.getElementById('mutirao');
+  if(mutirao){  
+    mutirao = mutirao.options[mutirao.selectedIndex].value; 
+    selectVara = mutirao;
+    //listarMutirao(mutirao);
+  }
+}
 
 function listarVaraComPauta(mutiroes){
   if (mutiroes.length > 0){
@@ -383,11 +332,11 @@ function formatarData(LocalDate, char){
   if(LocalDate !== null){
     if(LocalDate.indexOf("-") == 4 && LocalDate.length == 10){
       LocalDate= LocalDate.substring(8, 10)+char+LocalDate.substring(5, 7)+char+LocalDate.substring(0, 4);
-      //console.log("- posi 4: "+ LocalDate);
+      // console.log("- posi 4: "+ LocalDate);
     }else{
       if(LocalDate.indexOf("-") == 2 && LocalDate.length == 10){
         LocalDate= LocalDate.substring(6, 10)+char+LocalDate.substring(3, 5)+char+LocalDate.substring(0, 2);
-        //console.log("- posi 4")
+        console.log("- posi 4")
       }
     }
   }
@@ -420,43 +369,12 @@ function formatarData(LocalDate, char){
 //   }
 // }
 
-// function verificarSeTemEscala(pautas){
-//   var vara = document.getElementById('vara');
-//   vara = vara.options[vara.selectedIndex].value;
 
-//   axios.get(baseURL + 'pautas/').then(response => {
-//     pautas = response.data;
-//     pautas = pautas.filter(item =>  item.vara == vara);
-//     console.log(pautas.length);
-//     if(pautas.length < 1){
-//       pautas = response.data;
-//     }
-//     else{   
-//       pautas.forEach(function(pauta){
-//         if(!pauta.mutirao){
-//           console.log("pauta sem mutirão");
-//         }else{
-//           if(!pauta.procurador){
-//             console.log("pauta sem pautista")
-
-//             var btnGerar = document.querySelector('.btnGerar');
-//             console.log(btnGerar)
-//             btnGerar.style.display = 'inline';
-//           }
-//         }
-//       });
-      
-//     }
-
-    
-//   }).catch(error => console.error(error));
-// }
-
-// function listaEspecificaListar(pautasEspecificas){
-//   var tabela = $('#dataTablePesquisa').DataTable();
-//   tabela.rows().remove().draw(); 
-//   pautasEspecificas.forEach(listar);
-// }
+function listaEspecificaListar(pautasEspecificas){
+  var tabela = $('#dataTablePesquisa').DataTable();
+  tabela.rows().remove().draw(); 
+  pautasEspecificas.forEach(listar);
+}
 
 function limparCampos(){
   document.getElementById("vara").value = "1ª Vara Federal";
@@ -504,55 +422,66 @@ $('#gerar').on( 'click', function () {
 
 });
 
-// $('#vara').on( 'click', function () {
-//   var vara = document.getElementById('vara');
-//   vara = vara.options[vara.selectedIndex].value;
+$('#vara').on( 'click', function () {
+  var vara = document.getElementById('vara');
+  vara = vara.options[vara.selectedIndex].value;
 
-//   axios.get(baseURL + 'pautas/').then(response => {
-//     pautas = response.data;
-//     pautas = pautas.filter(item =>  item.vara == vara);
-//     //console.log(pautas);
-//     if(pautas.length < 1)
-//       pautas = response.data;
-//     listaEspecificaListar(pautas);
-//     verificarSeTemEscala(pautas);
-//   }).catch(error => console.error(error));
+  listarVaras(vara);
 
-//   //getMutirao();
-// });
+  axios.get(baseURL + 'pautas/').then(response => {
+    pautas = response.data;
+    pautas = pautas.filter(item =>  item.vara == vara);
+    //console.log(pautas);
+    if(pautas.length < 1)
+      pautas = response.data;
+    listaEspecificaListar(pautas);
+  }).catch(error => console.error(error));
 
-// $('#mutirao').on( 'click', function () {
-//   var tabela = $('#dataTablePesquisa').DataTable();
-//   var idMutirao = document.getElementById('mutirao');
+  getMutirao(); 
+});
 
-//   idMutirao = idMutirao.options[idMutirao.selectedIndex].value;
-  
-//   exibirBtnGerar();
-//    // pautas = pautas.filter(item =>  item.vara == vara); 
-//     var pautasPorMutirao =[];
-//     pautas.forEach(function(pauta){
-//       var mutirao = pauta.mutirao;
-//       pautaJson.data = pauta.data;
-//       pautaJson.hora = pauta.hora;
-//       pautaJson.sala = pauta.sala;
-//       pautaJson.processo = pauta.processo;
-//       pautaJson.nomeParte = pauta.nomeParte;
-//       pautaJson.cpf = pauta.cpf;
-//       pautaJson.nomeAdvogado = pauta.nomeAdvogado;
-//       pautaJson.objeto = pauta.objeto;
-//       pautaJson.procurador = pauta.procurador;
-//       if(mutirao.id == idMutirao)
-//          pautasPorMutirao.push(pauta);
-//     });
-//     //console.log(pautasPorMutirao)
-//     if(idMutirao != "Vara selecionada sem Mutirão"){
+$('#mutirao').on( 'click', function () {
+  var procurador = pautas.procurador;
+  if(procurador == null){
+    pautas.procurador = "";
+  }else{
+    if( procurador.nomeProcurador)
+      pautas.procurador = procurador.nomeProcurador;
       
-//       tabela.rows().remove().draw(); 
-//       pautasPorMutirao.forEach(listar);
-//     }
-//    // 
+  }
+
+
+
+  var tabela = $('#dataTablePesquisa').DataTable();
+  var idMutirao = document.getElementById('mutirao');
+
+  idMutirao = idMutirao.options[idMutirao.selectedIndex].value;
   
-// });
+   // pautas = pautas.filter(item =>  item.vara == vara); 
+    var pautasPorMutirao =[];
+    pautas.forEach(function(pauta){
+      var mutirao = pauta.mutirao;
+      pautaJson.data = pauta.data;
+      pautaJson.hora = pauta.hora;
+      pautaJson.sala = pauta.sala;
+      pautaJson.processo = pauta.processo;
+      pautaJson.nomeParte = pauta.nomeParte;
+      pautaJson.cpf = pauta.cpf;
+      pautaJson.nomeAdvogado = pauta.nomeAdvogado;
+      pautaJson.objeto = pauta.objeto;
+      pautaJson.procurador = pauta.procurador;
+      if(mutirao.id == idMutirao)
+         pautasPorMutirao.push(pauta);
+    });
+    //console.log(pautasPorMutirao)
+    if(idMutirao != "Vara selecionada sem Mutirão"){
+      
+      tabela.rows().remove().draw(); 
+      pautasPorMutirao.forEach(listar);
+    }
+   // 
+  
+});
 
 $('#editarMutirao').click( function () {
 
@@ -573,7 +502,6 @@ $('#pesquisar').click( function () {
   pautaJson.data = document.querySelector('#data').value.trim();
   pautaJson.hora = document.querySelector('#hora').value.trim();
   pautaJson.sala = document.querySelector('#sala').value.trim();
-  pautaJson.data2 = document.querySelector("#data2").value.trim();
 
   pautaJson.vara = document.getElementById('vara').value;
   var procurador = {
