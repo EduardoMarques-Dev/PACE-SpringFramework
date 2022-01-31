4ec1c24c9973fd24687288910955bbf322487adf
mlobato447 <83079439+mlobato447@users.noreply.github.com>
2022-01-31 10:59:32 -0300

    feat: Adicionando popups de confirmação de exclusão(em andamento)

--- gestao-de-escala-frontend/js/services/pautista.js
+++ gestao-de-escala-frontend/js/services/pautista.js
@@ -155,17 +155,34 @@ $('#editar').on( 'click', function () {
   sessionStorage.setItem('nome', nome);
 });
 
+
+
+
+
+
 $('#excluir').on('click', function () {
 
   var table = $('#dataTable').DataTable();
   var pautista = table.row('.selected').data();
+  if (pautista){
+    $('#popupExcluir').modal();
+  }
+  // var nome = pautista[0];  
+  // //console.log(pautista[0])
+  // pautistaJson = procuradores.filter(item =>  item.nome == nome);
+  // var id = pautistaJson[0].id;// primeira pauta com o processo pesquisado
+  // //console.log(id);
+  // deletar(id);
+});
+
+$('#excluirPautista').click(() => {
+  var table = $('#dataTable').DataTable();
+  var pautista = table.row('.selected').data();
   var nome = pautista[0];  
-  //console.log(pautista[0])
   pautistaJson = procuradores.filter(item =>  item.nome == nome);
   var id = pautistaJson[0].id;// primeira pauta com o processo pesquisado
-  //console.log(id);
   deletar(id);
-});
+})
 
 $('#pesquisar').click( function () { 
   var inputNome = document.getElementById("nome-pautista").value.trim();
