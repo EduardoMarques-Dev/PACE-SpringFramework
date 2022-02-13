73002e1c1e5fdbd2415f7f93ab642a63967d50e8
Carlos Eduardo <e.marquesdev@gmail.com>
2022-02-12 23:25:03 -0300

    refactor: removido front-end do repositório

--- gestao-de-escala-frontend/ambiente/baseURL.js
+++
@@ -1,5 +0,0 @@
-export const baseURL='https://agu-nutec-escala.herokuapp.com/';
-
-//export const baseURL='http://localhost:8081/';
-
-export default baseURL;
--- gestao-de-escala-frontend/js/services/config.js
+++
@@ -1,7 +0,0 @@
-import baseURL from '../../ambiente/baseURL.js'
-
-export const http = axios.create({
-    baseURL: baseURL
-});
-
-export default { http };
